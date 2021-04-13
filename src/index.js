5d678369547dafa3213daf5caaa6cb3fb1949586
sravittxof <travis.fox90@gmail.come>
2021-04-13 06:48:17 -0400

    commit tree a234565a8bdce5045dd40740ecbf3f256f1a6b56

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+fetch(imgUrl)
+.then(resp => resp.json())
+.then(function(json) {
+    for (const imageLink of json) {
+        let imgElem = document.createElement('img');
+        imgElem.src = imageLink;
+        document.body.appendChild(imgElem);
+    }
+})
